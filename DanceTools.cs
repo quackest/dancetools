10660a1e5fa37ac9c1656671b0243fd71df54283
Quack <neonbear69@gmail.com>
2024-01-07 21:31:09 -0800

    Fix for mod managers
    Sometimes(?) mod managers would extract the assetbundle file inside the plugins folder and not the correct folder. This should address that hopefully

--- DanceTools.cs
+++ DanceTools.cs
@@ -59,28 +59,36 @@ namespace DanceTools
         //host
         internal static bool isHost;
 
-
-        public static string prefix = "."; //default
-
         public void Awake()
         {
             Instance = this;
-            Logger.LogInfo("DanceTools loaded :^]");
-            
             mls = BepInEx.Logging.Logger.CreateLogSource("DanceTools");
 
             //load assetbundles
-            AssetBundle assets = null;
-            try
+
+            //if dancetoolsconsole is not present in the DanceTools folder
+            //do a check inside the plugins folder
+            //Mod managers for some reason don't extract the folder correctly and 
+            //dump the assetbundle dirrectly into the plugins folder.
+            AssetBundle assets = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "DanceTools/dancetoolsconsole"));
+            if(assets != null)
             {
-                assets = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "DanceTools/dancetoolsconsole"));
-                consoleRef = assets.LoadAsset<GameObject>("assets/prefabs/dancetoolsconsole.prefab"); //dancetoolsconsolestripped
-            } catch (Exception)
+                consoleRef = assets.LoadAsset<GameObject>("assets/prefabs/dancetoolsconsole.prefab"); //dancetoolsconsole
+                mls.LogInfo("Loaded DanceTools AssetBundle");
+            } else
             {
-                mls.LogFatal("Couldn't load assets.. make sure you've installed everything correctly");
-                consoleRef = null;
+                mls.LogWarning("Failed to load DanceTools AssetBundle, trying fallback..");
+                assets = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "dancetoolsconsole"));
+                if(assets != null)
+                {
+                    consoleRef = assets.LoadAsset<GameObject>("assets/prefabs/dancetoolsconsole.prefab");
+                    mls.LogInfo("Loaded DanceTools AssetBundle from fallback");
+                } else
+                {
+                    mls.LogFatal("Failed to load DanceTools AssetBundle");
+                    return;
+                }
             }
-            
             //debug
             /*string temp = "";
             foreach(string asset in assets.GetAllAssetNames())
@@ -89,9 +97,7 @@ namespace DanceTools
             }
             mls.LogInfo(temp);
             */
-
             spawnableEnemies = new List<SpawnableEnemies>();
-
             //adding ui elements in game when game starts.
             if(consoleRef != null)
             {
@@ -99,17 +105,15 @@ namespace DanceTools
                 console.AddComponent<DTConsole>();
                 console.AddComponent<DTCmdHandler>();
                 console.hideFlags = HideFlags.HideAndDontSave; //important!!!
-
                 DontDestroyOnLoad(console);
-
                 InitConfig();
-
+                mls.LogInfo("DanceTools Loaded :^]");
+                harmony.PatchAll(typeof(DanceTools));
             } else
             {
                 mls.LogFatal("No console assets present!!!!\nPlease check that you've installed everything correctly!!");
             }
-            //harmony
-            harmony.PatchAll(typeof(DanceTools));
+            
         }
 
         private void InitConfig()
@@ -216,138 +220,6 @@ namespace DanceTools
             public bool isOutside;
         }
 
-        //on chat message sent
-        [HarmonyPatch(typeof(HUDManager), "SubmitChat_performed")]
-        [HarmonyPrefix]
-        static void ChatMessageSent(HUDManager __instance)
-        {
-            if (!isHost) return; //if not host, ignore 
-
-            //currentRound = RoundManager.Instance;
-            string text = __instance.chatTextField.text;
-
-            //mls.LogInfo($"{text}"); //debug
-            //check if prefix is used
-            if (!text.ToLower().StartsWith(prefix.ToLower())) return;
-
-            //get specific command
-            if (text.ToLower().StartsWith(prefix + "item"))
-            {
-                string[] msg = text.Split(' ');
-
-                if(msg.Length < 2)
-                {
-                    DMNotice("Item Spawner", "Usage: .item itemID amount value\nCheck Bepin Console");
-
-                    string itemPrint = "\nItem List (ID | Name)";
-
-
-                    AllItemsList itemList = StartOfRound.Instance.allItemsList;
-                    mls.LogInfo(itemList.itemsList.Count);
-
-                    for (int i = 0; i < itemList.itemsList.Count; i++)
-                    {
-                        itemPrint += $"\n{i} | {itemList.itemsList[i].itemName}";
-                    }
-                    mls.LogInfo($"{itemPrint}");
-                    CommandSent(__instance);
-                    return;
-                }
-                int value = 0;
-                int amount = 1;
-                int index = int.Parse(msg[1]);
-
-                //check if item is in the AllItemsList, if not, ignore it
-                if(index > StartOfRound.Instance.allItemsList.itemsList.Count || index < 0)
-                {
-                    DMNotice($"Invalid Item ID ({index})", "Item doesn't exist in master item list");
-                    CommandSent(__instance);
-                    return;
-                }
-                //.item id amount value
-                if(msg.Length > 2)
-                {
-                    amount = int.Parse(msg[2]);
-                }
-                
-                //spawn multiple items
-                for(int i  = 0; i < amount; i++)
-                {
-                    GameObject obj = Instantiate(StartOfRound.Instance.allItemsList.itemsList[index].spawnPrefab, GameNetworkManager.Instance.localPlayerController.transform.position, Quaternion.identity);
-                    obj.GetComponent<GrabbableObject>().fallTime = 0f;
-                    obj.GetComponent<NetworkObject>().Spawn();
-
-                    //set cost for item
-                    if(msg.Length > 3)
-                    {
-                        value = int.Parse(msg[3]);
-                    }
-                    obj.AddComponent<ScanNodeProperties>().scrapValue = value; //attach scanning node for value
-                    obj.GetComponent<GrabbableObject>().SetScrapValue(value); //give value to it
-                    //item 65 tragedy 
-                }
-                DMNotice($"Item ({index})", $"{StartOfRound.Instance.allItemsList.itemsList[index].itemName} at value {value}");
-                CommandSent(__instance);
-            }
-
-            //
-            if (text.ToLower().StartsWith(prefix + "enemy"))
-            {
-                string[] msg = text.Split(' ');
-                //info message:
-                if(msg.Length < 2)
-                {
-                    string consoleInfo = "\n";
-                    string spawnableEnemies = "";
-
-                    if(currentRound.currentLevel.Enemies.Count <= 0)
-                    {
-                        DMNotice("No Enemies", "No enemies to spawn in this level");
-                        CommandSent(__instance);
-                        return;
-                    }
-
-                    for(int i = 0; i < currentRound.currentLevel.Enemies.Count; i++)
-                    {
-                        consoleInfo += $"\n{i} | {currentRound.currentLevel.Enemies[i].enemyType.enemyName}";
-                        spawnableEnemies += $"({i}) {currentRound.currentLevel.Enemies[i].enemyType.enemyName}, ";
-                    }
-                    mls.LogInfo(consoleInfo);
-                    DMNotice("Enemy Spawn Command", $".enemy id \n{spawnableEnemies}");
-                    CommandSent(__instance);
-                    return;
-                }
-                try
-                {
-                
-                    int index = int.Parse(msg[1]);
-
-                //default = in vents
-                Vector3 spawnPos = currentRound.allEnemyVents[UnityEngine.Random.Range(0, currentRound.allEnemyVents.Length)].floorNode.position;
-                string noticeBody = "Spawned in a random vent inside";
-
-                //spawn it in a random vent
-                if(msg.Length > 2)
-                {
-                    if (msg[2] == "onme")
-                    {
-                        spawnPos = GameNetworkManager.Instance.localPlayerController.transform.position;
-                        noticeBody = "Spawned on top of you";
-                    }
-                }
-                currentRound.SpawnEnemyOnServer(spawnPos, 0f, index);
-
-                DMNotice($"{currentRound.currentLevel.Enemies[index].enemyType.enemyName} Spawned!", noticeBody);
-                CommandSent(__instance);
-                } 
-                catch(Exception e) 
-                {
-                    DMNotice("Couldn't spawn enemy", "Can't spawn enemies when not landed. Check console for more info");
-                    mls.LogInfo($"error: {e.Message}");
-                    CommandSent(__instance);
-                }
-            }
-        }
 
         public static int CheckInt(string input)
         {
@@ -376,13 +248,8 @@ namespace DanceTools
             }
         }
 
-        //do something when command is sent
-        static void CommandSent(HUDManager ins)
-        {
-            ins.chatTextField.text = ""; //hide the command message
-        }
 
-        //to display messages
+        //to display messages on hud (like warning message)
         public static void DMNotice(string title, string msg)
         {
             HUDManager.Instance.DisplayTip(title, msg);
