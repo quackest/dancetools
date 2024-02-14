64509e3168ab446be7f23d094d14c4d40ca16bd2
dancemoon <48807848+quackest@users.noreply.github.com>
2024-02-13 21:16:35 -0800

    gulp forgor to remove debug print text

--- DTWidget.cs
+++ DTWidget.cs
@@ -125,7 +125,7 @@ namespace DanceTools
             outputBackground.color = DanceTools.consoleOutputFieldColor;
             inputBackground.color = DanceTools.consoleInputFieldColor;
 
-            PushTextToOutput($"{DanceTools.consoleInputFieldColor}");
+            //PushTextToOutput($"{DanceTools.consoleInputFieldColor}");
             
         }
 
